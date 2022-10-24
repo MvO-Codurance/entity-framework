5140823319e7b438b2722805902aa14ddd9e649d
Mike van Oorschot <michael.oorschot@codurance.com>
2022-10-24 14:11:32 +0100

    Complete tutorial to end of part 3.

--- ContosoUniversity/Pages/Students/Create.cshtml.cs
+++ ContosoUniversity/Pages/Students/Create.cshtml.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using ContosoUniversity.Data;
 using ContosoUniversity.Models;
 
 namespace ContosoUniversity.Pages.Students
